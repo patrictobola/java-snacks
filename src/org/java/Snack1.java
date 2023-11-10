a3648bb84eeb14682fcfaf646a66959071f8ddbd
patrictobola <tobola92@gmail.com>
2023-11-10 11:20:46 +0100

    snack1

--- src/org/java/Snack1.java
+++ src/org/java/Snack1.java
@@ -1,9 +1,18 @@
 package org.java;
 
+import java.util.Scanner;
+
 public class Snack1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		
+		System.out.print("Inserisci un numero: ");
+		String value = in.nextLine();
+		int digit = Integer.valueOf(value);
+		
+		if(digit % 2 == 0) System.out.println("Il numero inserito è paro ed è: " + digit);
+		else System.out.println("Il numero inserito è disparo, verrà printato il primo numero paro successivo: " + (digit + 1));
 
 	}
 

f5bda048a448e9cb226758f96752c2adb4beea2a
patrictobola <tobola92@gmail.com>
2023-11-10 13:01:53 +0100

    snack5 da finire, manca il ciclo che chiede nuova stringa finche non si mette 0

---
+++ src/org/java/Snack5.java
@@ -0,0 +1,34 @@
+package org.java;
+
+import java.util.Scanner;
+
+public class Snack5 {
+
+	public static void main(String[] args) {
+
+		Scanner in = new Scanner(System.in);
+		
+		System.out.print("Inserisci una o più parole: ");
+		String phrase = in.nextLine();
+		
+		int pLen = phrase.length();
+
+//		variabili contatore
+		int digits = 0;
+		int letters = 0;
+		int others = 0;
+		
+		for(int x = 0; x < pLen; x++) {
+			char c = phrase.charAt(x);
+			if (Character.isDigit(c)) digits++;
+			else if (Character.isLetter(c)) letters++;
+			else others++;
+		}
+		
+		System.out.println("Nelle parole che ci hai dato ci sono:");
+		System.out.println("N." + digits + " di numeri");
+		System.out.println("N." + letters + " di lettere");
+		System.out.println("N." + others + " di caratteri non alfanumerici");
+	}
+
+}

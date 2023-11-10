b0e2144dcb3a42197ff900bc98a895e5f760c911
patrictobola <tobola92@gmail.com>
2023-11-10 17:14:30 +0100

    fixed arrays in snack 9

--- src/org/java/Snack9.java
+++ src/org/java/Snack9.java
@@ -1,24 +1,30 @@
 package org.java;
 
-import java.util.ArrayList;
-
 public class Snack9 {
 
 	public static void main(String[] args) {
 
 		int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 		int nLen = numbers.length;
+
+		int[] even = new int[10];
+		int[] odd = new int[10];
 		
-//		Ho trovato su google questi array dove si può gestire dinamicamente la dimensione 
-		ArrayList<Integer> even = new ArrayList<>();
-		ArrayList<Integer> odd = new ArrayList<>();
+		
+		int evenIndex = 0;
+		int oddIndex = 0;
 		
 		
-		for(int x = 0; x < nLen; x++) {
-			if(numbers[x] % 2 == 0) even.add(numbers[x]);
-			else odd.add(numbers[x]);
-		}
-		System.out.println(even);
+		 for (int x = 0; x < nLen; x++) {
+	            if (numbers[x] % 2 == 0) {
+	                even[evenIndex] = numbers[x];
+	                evenIndex++;
+	            } else {
+	                odd[oddIndex] = numbers[x];
+	                oddIndex++;
+	            }
+	        }
+		System.out.println(odd[1]);
 	}
 
 }
